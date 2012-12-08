819b3ee966688aaa16c037a6f63d8a4d7490dd50
Joshua Rieken <joshua@joshuarieken.com>
2012-12-08 12:28:14 -0600

    Use fallback blank avatar image if no avatar_url or gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,12 @@
 module UsersHelper
-
   def avatar_url(user)
-    if user.try(:avatar_url).present?
-      user.try(:avatar_url)
-    else
+    if user.avatar_url.present?
+      user.avatar_url
+    elsif user.email.present?
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
       "http://gravatar.com/avatar/#{gravatar_id}.png?s=80"
+    else
+      image_path "blank_avatar.png"
     end
   end
-
 end
